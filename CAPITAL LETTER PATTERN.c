0c92187e07869f03050fa2e50d553c5d7654f2ef
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-07-21 14:56:14 +0530

    Create CAPITAL LETTER PATTERN.c

---
+++ CAPITAL LETTER PATTERN.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a Number >>");
+    scanf("%d", &a);
+    getchar();
+    for (int i=1;i<=a;i++)
+    {
+       for(int j=1;j<=a;j++)
+        {
+            int k = i + 64;
+            printf("%c ", k);
+        }
+        printf("\n");
+    }
+    getchar();
+   // getchar();
+}
