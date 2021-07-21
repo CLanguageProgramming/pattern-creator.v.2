84fc5724b4e5e8e7fb4e1eb3d6a31defb709e1c4
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-07-21 14:55:14 +0530

    Create Main2.c

---
+++ Main2.c
@@ -0,0 +1,20 @@
+  
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a Number >>");
+    scanf("%d", &a);
+    getchar();
+    for (int i=1;i<=a;i++)
+    {
+       for(int j=1;j<=a;j++)
+        {
+            int k = i + 96;
+            printf("%c ", k);
+        }
+        printf("\n");
+    }
+    getchar();
+   // getchar();
+}
